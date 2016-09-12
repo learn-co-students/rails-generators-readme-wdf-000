9d3118215275909fff2c806121fe6d2de86a5fb5
Flatiron School <github email address>
2016-09-12 14:14:16 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  # resources :accounts
+  # get 'admin/dashboard'
+  #
+  # get 'admin/stats'
+  #
+  # get 'admin/financials'
+  #
+  # get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  resources :categories
+  # resources :categories
 end
